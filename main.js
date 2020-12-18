7f84907fb5f405d54a688b96b285af96aab673fb
Ethan Weisberg <etweisberg@packer.edu>
2020-12-17 20:58:38 -0500

    add item menu

--- main.js
+++ main.js
@@ -1,34 +1,86 @@
-const {app, BrowserWindow} = require('electron')
-const path = require('path')
-const url = require('url');
+const { app, BrowserWindow, Menu } = require("electron");
+const path = require("path");
+const url = require("url");
 
-function createWindow () {
+function createWindow() {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
     webPreferences: {
-      preload: path.join(__dirname, 'preload.js')
-    }
-  })
+      preload: path.join(__dirname, "preload.js"),
+    },
+  });
 
   // and load the index.html of the app.
-  mainWindow.loadURL(url.format({
-    pathname: path.join(__dirname,'mainWindow.html'),
-    protocol:'file',
-    slashes: true
-  }))
+  mainWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "mainWindow.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
+  mainWindow.on('closed',function(){
+    app.quit();
+  })
+}
+
+//Handle add item menu option
+function createAddWindow(){
+  const addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title:"Add List Item"
+  });
+
+  // and load the index.html of the app.
+  addWindow.loadURL(
+    url.format({
+      pathname: path.join(__dirname, "addWindow.html"),
+      protocol: "file",
+      slashes: true,
+    })
+  );
 }
 
 app.whenReady().then(() => {
-  createWindow()
-  
-  app.on('activate', function () {
-    if (BrowserWindow.getAllWindows().length === 0) createWindow()
-  })
-})
+  createWindow();
 
-app.on('window-all-closed', function () {
-  if (process.platform !== 'darwin') app.quit()
-})
+  app.on("activate", function () {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
 
+  //Build menu from template
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  //Insert menu
+  Menu.setApplicationMenu(mainMenu);
+});
+
+app.on("window-all-closed", function () {
+  if (process.platform !== "darwin") app.quit();
+});
+
+//Menu template
+const mainMenuTemplate = [
+  {
+    label: "File",
+    submenu: [
+      {
+        label: "Add Item",
+        click(){
+          createAddWindow();
+        }
+      },
+      {
+        label: "Clear Items",
+      },
+      {
+        label: "Quit",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        click() {
+          app.quit();
+        },
+      },
+    ],
+  },
+];
